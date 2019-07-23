290624aa611ca85cbffb262c02a4278369d66801
BlandineK <blandineuwase222@gmail.com>
2019-07-23 11:32:38 +0200

    add the id called boy,girl

--- java.js
+++ java.js
@@ -1,3 +1,8 @@
 function myResult() {
-
+var day=document.getElementById(blandine1).Value;
+var month=document.getElementById(blandine2).value;
+var year=document.getElementById(blandine3).value;
+var gender=document.getElementById(boy,girl).value;
+var submit=
+var Submit=Math.floor(submit);
 }
