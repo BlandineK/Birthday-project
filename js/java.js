2a36c2634e429d9cf391c34900705c5aa1b63698
BlandineK <blandineuwase222@gmail.com>
2019-07-24 11:41:19 +0200

    add changes made to java.js

--- js/java.js
+++ js/java.js
@@ -2,40 +2,45 @@ function myResult() {
 var day=parseInt(document.getElementById("blandine1").value);
 var month=parseInt(document.getElementById("blandine2").value);
 var year=parseInt(document.getElementById("blandine3").value);
-var gender=document.querySelector("boy","girl").value;
+var gender=document.querySelector("input[name='gender']:checked").value;
 
-
+console.log(gender);
 var cc=(year-1)/100+1;
 var submit=( ( (cc/4) -2*cc-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day )%7 ;
 var submit=Math.floor(submit);
 console.log(submit);
 
-var get=["kwasi","kwadwo","kwabena","Kwaku","yaw","kofi","kwame"]
+var get=[Sunday="Kwasi",Monday="Kwadwo",Tuesday="Kwabena",Wednesday="Kwaku",Thursday="Yaw",Friday="Kofi",Saturday="kwame"]
 if(day>0 && day<=31 && month>0 && month <12 && year>0){
 if ( submit==0 && gender=="Male" ){
 
-    document.getElementById("add").innerHTML=get[0];
+    document.getElementById("add").innerHTML=("The akan name is " + get[0]);
 }
 else if (submit==1 && gender=="Male"){
 
-    document.getElementById("add").innerHTML=get[1];
+    document.getElementById("add").innerHTML=("The akan name is " + get[1]);
+}
+else if ( submit==2 && gender=="Male"){
+
+    document.getElementById("add").innerHTML=("The akan name is " + get[2]);
 }
 else if ( submit==3 && gender=="Male"){
 
-    document.getElementById("add").innerHTML=get[3];
+    document.getElementById("add").innerHTML=("The akan name is " + get[3]);
 }
 else if ( submit==4 && gender=="Male"){
 
-    document.getElementById("add").innerHTML=get[4];
+    document.getElementById("add").innerHTML=("The akan name is " + get[4]);
 }
 else if ( submit==5 && gender=="Male"){
 
-    document.getElementById("add").innerHTML=get[5];
+    document.getElementById("add").innerHTML=("The akan name is " + get[5]);
 }
 else if ( submit==6 && gender=="Male"){
 
-    document.getElementById("add").innerHTML=get[6];
+    document.getElementById("add").innerHTML=("The akan name is " + get[6]);
 }
+var get=[Sunday="Akosua",Monday="Adwoa",Wednesday="Akua",Thursday="Yaa",Friday="Afua",Saturday="Ama"]
     if ( submit==0 && gender=="Female" ){
     
         document.getElementById("add").innerHTML=get[0];
@@ -59,6 +64,10 @@ else if ( submit==6 && gender=="Male"){
     else if ( submit==6 && gender=="Female"){
     
         document.getElementById("add").innerHTML=get[6];
+    }
+    else{
+        alert("waaw");
+
     }
 }
 }
