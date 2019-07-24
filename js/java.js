46ce165b6b22473df25ab054e198f3a2b70714a6
BlandineK <blandineuwase222@gmail.com>
2019-07-24 12:32:33 +0200

    add the last changes made to java.js

--- js/java.js
+++ js/java.js
@@ -14,60 +14,60 @@ var get=["Kwasi : Sunday","Kwadwo : Monday","Kwabena : Tuesday","Kwaku : Wednesd
 if(day>0 && day<=31 && month>0 && month <12 && year>0){
 if ( submit==0 && gender=="Male" ){
 
-    document.getElementById("add").innerHTML=("The akan name is  and you were born on Sunday" + get[0]);
+    document.getElementById("add").innerHTML=("The Akan name is  " + get[0]);
 }
 else if (submit==1 && gender=="Male"){
 
-    document.getElementById("add").innerHTML=("The akan name is " + get[1]);
+    document.getElementById("add").innerHTML=("The Akan name is " + get[1]);
 }
 else if ( submit==2 && gender=="Male"){
 
-    document.getElementById("add").innerHTML=("The akan name is " + get[2]);
+    document.getElementById("add").innerHTML=("The Akan name is " + get[2]);
 }
 else if ( submit==3 && gender=="Male"){
 
-    document.getElementById("add").innerHTML=("The akan name is " + get[3]);
+    document.getElementById("add").innerHTML=("The Akan name is " + get[3]);
 }
 else if ( submit==4 && gender=="Male"){
 
-    document.getElementById("add").innerHTML=("The akan name is " + get[4]);
+    document.getElementById("add").innerHTML=("The Akan name is " + get[4]);
 }
 else if ( submit==5 && gender=="Male"){
 
-    document.getElementById("add").innerHTML=("The akan name is " + get[5]);
+    document.getElementById("add").innerHTML=("The Akan name is " + get[5]);
 }
 else if ( submit==6 && gender=="Male"){
 
-    document.getElementById("add").innerHTML=("The akan name is " + get[6]);
+    document.getElementById("add").innerHTML=("The Akan name is " + get[6]);
 }
 var get=["Akosua : Sunday","Adwoa : Monday","Abenaa: Tuesday","Akua : Wednesday","Yaa : Thursday","Afua : Friday","Ama : Saturday"]
     if ( submit==0 && gender=="Female" ){
     
-        document.getElementById("add").innerHTML=("The akan name is " + get[0]);
+        document.getElementById("add").innerHTML=("The Akan name is " + get[0]);
     }
     else if ( submit==1 && gender=="Female"){
 
-        document.getElementById("add").innerHTML=("The akan name is " + get[1]);
+        document.getElementById("add").innerHTML=("The Akan name is " + get[1]);
     }
     else if (submit==2 && gender=="Female"){
     
-        document.getElementById("add").innerHTML=("The akan name is " + get[2]);
+        document.getElementById("add").innerHTML=("The Akan name is " + get[2]);
     }
     else if ( submit==3 && gender=="Female"){
     
-        document.getElementById("add").innerHTML=("The akan name is " + get[3]);
+        document.getElementById("add").innerHTML=("The Akan name is " + get[3]);
     }
     else if ( submit==4 && gender=="Female"){
     
-        document.getElementById("add").innerHTML=("The akan name is " + get[4]);
+        document.getElementById("add").innerHTML=("The Akan name is " + get[4]);
     }
     else if ( submit==5 && gender=="Female"){
     
-        document.getElementById("add").innerHTML=("The akan name is " + get[5]);
+        document.getElementById("add").innerHTML=("The Akan name is " + get[5]);
     }
     else if ( submit==6 && gender=="Female"){
     
-        document.getElementById("add").innerHTML=("The akan name is " + get[6]);
+        document.getElementById("add").innerHTML=("The Akan name is " + get[6]);
     }
     else{
         alert("Good!!");
